746fed140d2d6bb2810be0000a52a43c138243a3
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-03-02 15:01:08 +0300

    Structure of class

--- estackexceptions.h
+++ estackexceptions.h
@@ -1,11 +1,35 @@
 #ifndef ESTACKEXCEPTIONS_H
 #define ESTACKEXCEPTIONS_H
-
+#include <string.h>
 
 class EStackException
 {
+private:
+    char* message;
 public:
-    EStackException();
+    EStackException(const char* arg_message)
+    {
+        message = new char[strlen(arg_message)+1];
+        strcpy(message,arg_message);
+
+    }
+    EStackException(const EStackException& arg)
+    {
+        message = new char[strlen(arg.message)+1];
+        strcpy(message,arg.message);
+    }
+    ~EStackException()
+    {
+        delete message;
+    }
+    const char* what() const { return message; }
+};
+
+class EStackEmpty : public EStackException
+{
+public:
+    explicit EStackEmpty(const char* arg = "Stack is Empty") : EStackException(arg) {}
+    explicit EStackEmpty(const EStackEmpty& arg) : EStackException(arg) {}
 };
 
 #endif // ESTACKEXCEPTIONS_H
