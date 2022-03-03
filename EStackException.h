eef5ec29ab07d1d7575a31866a748ced48467ffb
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-03-03 09:11:15 +0300

    Remarks2

---
+++ EStackException.h
@@ -0,0 +1,36 @@
+#ifndef ESTACKEXCEPTION_H
+#define ESTACKEXCEPTION_H
+#include <string.h>
+
+class EStackException
+{
+private:
+    char* message;
+public:
+    EStackException(const char* arg_message)
+    {
+        message = new char[strlen(arg_message)+1];
+        for (int j = 0; j < strlen(arg_message) + 1; j++)
+                    message[j] = arg_message[j];
+    }
+    EStackException(const EStackException& arg)
+    {
+        message = new char[strlen(arg.message)+1];
+        for (int j = 0; j < strlen(arg.message) + 1; j++)
+                    message[j] = arg.message[j];
+    }
+    ~EStackException()
+    {
+        delete message;
+    }
+    const char* what() const { return message; }
+};
+
+class EStackEmpty : public EStackException
+{
+public:
+    explicit EStackEmpty(const char* arg = "Stack is Empty") : EStackException(arg) {}
+    explicit EStackEmpty(const EStackEmpty& arg) : EStackException(arg) {}
+};
+
+#endif // ESTACKEXCEPTION_H
--- main.cpp
+++ main.cpp
@@ -6,10 +6,16 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+<<<<<<< 3124eb98bf561978b0064eadc173fe55c4b876aa
 
     for(int i=0;i<11;i++)
         //тут должно быть заполнение стека
 
+=======
+    Stack<int> stack; //создаём стек
+    for(int i=0; i<11;i++)
+        stack.Push(i); //заполнение стека
+>>>>>>> Remarks2
     for(int i=0; i<12; i++)
         //"опустошение" стека
     return a.exec();
