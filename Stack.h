115bd89bfb26400de5d2b578bb8c99a29fd712f2
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-03-16 22:27:20 +0300

    Adding constructors in Stack

--- Stack.h
+++ Stack.h
@@ -1,7 +1,42 @@
 #ifndef STACK_H
 #define STACK_H
+<<<<<<< 3124eb98bf561978b0064eadc173fe55c4b876aa
 #include "estackexceptions.h"
 
+=======
+#include <QDebug>
+#include "EStackException.h"
+#include <iostream>
+
+//класс стека - шаблонный
+
+template<typename T>
+class Stack
+{
+private:
+    class Node
+    {
+    public:
+        T value;
+        Node* next;
+        Node(T value, Node* next = nullptr)
+        {
+            this->value = value;
+            this->next = next;
+        }
+    };
+    Node* top = nullptr;
+    int size = 0;
+public:
+    void Push(T value);
+    T Pop();
+    int Len();
+    //добавляем конструкторы
+    Stack<T>& operator=(const Stack<T>&); //присваивание
+    Stack<T>(const Stack<T>& stack); //копирование
+    Stack<T>(){};
+};
+>>>>>>> Adding constructors in Stack
 
 
 <<<<<<< 3124eb98bf561978b0064eadc173fe55c4b876aa
@@ -39,5 +74,42 @@ int Stack<T>::Len()
     return size;
 }
 >>>>>>> Remark
+
+template<typename T>
+Stack<T>& Stack<T>::operator=(const Stack<T>& stack) //реализация оператора присваивания
+{
+    Stack<T> inverse;
+    Node *node = stack.top;
+    for (int i = 0; i < stack.size; i++)
+    {
+        inverse.Push(node->value);
+        node = node->next;
+    }
+    node = inverse.top;
+    for (int i = 0; i < stack.size; i++)
+    {
+        this->Push(node->value);
+        node = node->next;
+    }
+    return *this;
+}
+
+template<typename T>
+Stack<T>::Stack(const Stack<T> &stack)
+{
+    Stack<T> inverse;
+    Node *node = stack.top;
+    for (int i = 0; i < stack.size; i++)
+    {
+        inverse.Push(node->value);
+        node = node->next;
+    }
+    node = inverse.top;
+    for (int i = 0; i < stack.size; i++)
+    {
+        this->Push(node->value);
+        node = node->next;
+    }
+}
 
 #endif // STACK_H
