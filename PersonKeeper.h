8dad21b72501a39d740a3ba3747d93a776d889ed
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-03-16 21:42:09 +0300

    Methods for the PersonKeeper

--- PersonKeeper.h
+++ PersonKeeper.h
@@ -1,6 +1,7 @@
 #ifndef PERSONKEEPER_H
 #define PERSONKEEPER_H
 #include "Stack.h"
+#include "Person.h"
 #include <fstream>
 
 class PersonKeeper
@@ -14,6 +15,9 @@ public:
         static PersonKeeper PK;
         return PK;
     }
+    // методы класса PersonKeeper
+    Stack<Person> readPersons(std::fstream&); //будет возвращает стек, который уже заполнен ФИО
+    void wridePerson(const Stack<Person> &); //будет записывать в поток из стека ФИО
 };
 
 #endif // PERSONKEEPER_H
