a2d815c04798388f1ea9571979383db9aee2bff0
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-03-18 14:40:01 +0300

    String for Person (1)

--- Person.cpp
+++ Person.cpp
@@ -21,3 +21,16 @@ Person::Person(Person const& person)
     this->first_name = first_name;
     this->patronymic = patronymic;
 }
+
+//Нужно возвратить char* в виде строки
+char* Person::String()
+{
+    int length = last_name.length() + first_name.length() + patronymic.length() + 3; //+3 для пробелов и перехода на новую строку
+    char* name = new char[length];
+    long long unsigned i = 0;
+    for(i = 0; i < last_name.length(); i++)
+        name[i] = last_name[i];
+    name[i] = ' ';
+    i++;
+    // i общая для записи, но не для подсчитывания символов в каждом слове ...
+}
