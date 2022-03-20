a7386f5bcb6ed6bffff2cfc55fbdd188aeab0a23
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-03-20 21:48:12 +0300

    readPersons for PersonKeeper

--- PersonKeeper.cpp
+++ PersonKeeper.cpp
@@ -2,7 +2,20 @@
 
 Stack<Person> PersonKeeper::readPersons()
 {
-
+    this->stream->exceptions(std::ios_base::eofbit);
+    std::string FIO[9];
+    Stack<Person> stack;
+    for (; ;)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try{
+                    *this->stream >> FIO[i];
+                }catch (const std::ios_base::failure&){
+                    return stack;
+                }
+            }
+        }
 }
 
 void PersonKeeper::writePersons(const Stack<Person> &input)
