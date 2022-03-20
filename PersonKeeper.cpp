f2d56da5f2d0b2b83da408bdff82ee2cd96d77ef
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-03-21 00:49:13 +0300

    Finished PersonKeeper.cpp

--- PersonKeeper.cpp
+++ PersonKeeper.cpp
@@ -1,24 +1,25 @@
 #include "PersonKeeper.h"
 
+// преобразование потока в поле класса PersonKeeper
 Stack<Person> PersonKeeper::readPersons()
 {
     this->stream->exceptions(std::ios_base::eofbit);
-    std::string name[3];
     Stack<Person> stack;
+    std::string fio[3];
     for (; ;)
     {
         for (int i = 0; i < 3; i++)
         {
             try
             {
-                *this->stream >> name[i];
+                *this->stream >>  fio[i]; // заполняем fio[i] ФИО
             }
             catch (const std::ios_base::failure&)
             {
                 return stack;
             }
         }
-        stack.Push(Person(name[0], name[1], name[2]));
+        stack.Push(Person( fio[0],  fio[1],  fio[2])); // отправляем в стек
      }
 }
 
@@ -27,9 +28,9 @@ void PersonKeeper::writePersons(const Stack<Person> &input)
     Stack<Person> stack = input;
     for(; 0 < stack.Len();)
     {
-        *this->stream << stack.Pop().String();
+        *this->stream << stack.Pop().String(); // записываем в файл все ФИО
         if(stack.Len() != 0)
-            stream->put('\n');
+            stream->put('\n'); // переходим на новую строку
     }
     stream->close();
 }
