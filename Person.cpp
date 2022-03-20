7759a4a35bc688117b4a37e4f850be528e8427ea
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-03-20 21:22:57 +0300

    Ready Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -3,41 +3,50 @@
 Person::Person(const std::string & last_name, const std::string & first_name, const std::string & patronymic)
 {
     this->last_name = last_name;
+
     this->first_name = first_name;
+
     this->patronymic = patronymic;
 }
 
 Person& Person::operator=(const Person &person)
 {
-    this->last_name = last_name;
-    this->first_name = first_name;
-    this->patronymic = patronymic;
+    this->last_name = person.last_name;
+
+    this->first_name = person.first_name;
+
+    this->patronymic = person.patronymic;
+
     return *this;
 }
 
 Person::Person(Person const& person)
 {
-    this->last_name = last_name;
-    this->first_name = first_name;
-    this->patronymic = patronymic;
+    this->last_name = person.last_name;
+
+    this->first_name = person.first_name;
+
+    this->patronymic = person.patronymic;
 }
 
 //Нужно возвратить char* в виде строки
 char* Person::String()
 {
-    int length = last_name.length() + first_name.length() + patronymic.length() + 3; //+3 для пробелов и перехода на новую строку
+    int length = last_name.length() + first_name.length() + patronymic.length() + 3; //+3 для пробелов и знака о том, что строка окончена.
     char* FIO = new char[length];
     long long unsigned i = 0;
     for(i = 0; i < last_name.length(); i++)
         FIO[i] = last_name[i];
     name[i] = ' ';
     i++;
+    // переменная i нужна для каждого из слов ФИО, а j понадобится для отдельно взятого слова
     int j = 0;
-    for(; i < first_name.length(); i++; j++)
+    for(; j < first_name.length(); i++; j++)
         FIO[i] = first_name[j];
     name[i] = ' ';
     i++;
-    for(; i < first_name.length(); i++; j++)
+    for(j = 0; j < patronymic.length(); i++; j++)
         FIO[i] = patronymic[j];
     name[i] = '\0'; // конец для строки ФИО
+    return FIO;
 }
