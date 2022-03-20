e5469221ad16584efcd552df21e15eb6721b06f3
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-03-20 21:41:04 +0300

    writePersons for PersonKeeper

--- PersonKeeper.cpp
+++ PersonKeeper.cpp
@@ -7,5 +7,12 @@ Stack<Person> PersonKeeper::readPersons()
 
 void PersonKeeper::writePersons(const Stack<Person> &input)
 {
-
+    Stack<Person> stack = input;
+    for(; 0 < stack.Len();)
+    {
+        *this->stream << stack.Pop().String();
+        if(stack.Len() != 0)
+            stream->put('\n');
+    }
+    stream->close();
 }
